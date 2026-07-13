8e0f2ca7492e1f53734098b2ea328ef52b13130e
Ayush Gauniyal <144988656+agpersonal6104@users.noreply.github.com>
2026-07-14 00:00:49 +0530

    Initial commit

--- missing values.py
+++ missing values.py
@@ -9,3 +9,19 @@ import numpy as np
 
 # Working with missing values
 
+a=np.array([1,2,3,4,np.nan,6])
+print(a)
+
+print(np.isnan(a))
+
+print(a[~np.isnan(a)])
+
+# Plotting graphs using numpy
+x=np.linspace(-10,10,100)
+y=x;
+
+import matplotlib.pyplot as plt
+
+x=np.linspace(-10,10,100)
+y=x**2
+plt.plot(x,y)
