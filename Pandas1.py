d6504ed2abce4e34692540b3f79f02016c857e9c
Ayush Gauniyal <144988656+agpersonal6104@users.noreply.github.com>
2026-07-18 20:17:58 +0530

    Pandas Series Practice

--- Pandas1.py
+++ Pandas1.py
@@ -32,5 +32,53 @@ marks={
        'Chemistry':57
 }
 
-table2=pd.Series(marks)
+table2=pd.Series(marks,name="Ayush ke marks!")
 print(table2)
+
+# Attributes
+print(table2.size)
+print(table2.dtype)
+print(table2.name)
+print(table2.is_unique)
+print(table2.index)
+print(table2.values)
+
+# Series using CSV
+df=pd.read_csv(r'C:\Users\emper\Downloads\bollywood.csv',index_col='movie')
+print(type(df))
+df=df.squeeze()
+print(type(df))
+
+print("----------------------------------")
+
+# Series Methods
+
+print(df.head())
+print("----------------------------------")
+print(df.tail())
+print("----------------------------------")
+print(df.sample(5)) #random selection
+print("----------------------------------")
+print(df.value_counts()) # Frequency in descending order
+print("----------------------------------")
+print(df.sort_values().head(1).values[0]) # Sort values
+print("----------------------------------")
+print(df.sort_index()) # Sort index
+
+print("----------------------------------")
+
+# Series Maths functions
+
+print(df.count())
+print(df.sum())
+# product, mean, median and mode, min/max can be applied to int
+
+print("----------------------------------")
+print(df.describe)
+
+print("----------------------------------")
+
+# Series Indexing
+x=pd.Series([12,13,14,35,34,57,59,79])
+print(x[1]) #series only works in +ve indexing
+print(df.iloc[0])
